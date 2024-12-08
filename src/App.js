47c961b24aa460e84ec4f706da46a6e618e8b6ba
homp6 <homp@unitec.edu>
2024-12-08 17:47:27 -0600

    El fix entero del bug aproximado

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2"
 import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
 const { Content } = Layout;
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
             <Route path="/" element={<Page1 />} />
             <Route path="/page2" element={<Page2 />} />
             <Route path="/page3" element={<Page3 />} />
+            <Route path="/page4" element={<Page4 />} />
           </Routes>
         </Content>
       </Layout>
